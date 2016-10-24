4bd8c9748fcb2570facdb6daf5d9ea7f06d626cf
HPL5201314 <997596430@qq.com>
2016-10-24 18:17:33 +0800

    Initial commit

---
+++ app/src/main/java/com/example/hpl/alarmclock/activities/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.hpl.alarmclock.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2016/10/24.
+ */
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/hpl/alarmclock/activities/TimerOnTimeActivity.java
@@ -0,0 +1,8 @@
+package com.example.hpl.alarmclock.activities;
+
+/**
+ * Created by Administrator on 2016/10/24.
+ */
+
+public class TimerOnTimeActivity {
+}
